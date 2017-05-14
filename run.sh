f8fb7c48c853c46e1f5554919418ba6f79d97240
Eric Keller <keller.eric@gmail.com.com>
2017-05-14 17:31:44 +0200

    fixup newline

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ unset DEBIAN_FRONTEND
 export LOG=/var/log/jitsi/jvb.log
 
 if [ ! -f "$LOG" ]; then
-	
+
 	sed 's/#\ create\(.*\)/echo\ create\1 $JICOFO_AUTH_USER $JICOFO_AUTH_DOMAIN $JICOFO_AUTH_PASSWORD/' -i /var/lib/dpkg/info/jitsi-meet-prosody.postinst
 
 	dpkg-reconfigure jitsi-videobridge
