c4ea3d18bae633c59b77983a3748f54dd06b5681
Eric Keller <keller.eric@gmail.com.com>
2017-05-14 17:31:45 +0200

    add 5280 port forwarding

--- setup_forwarding.sh
+++ setup_forwarding.sh
@@ -4,6 +4,7 @@ INTERFACE=wlo1
 test -z $1 || TARGET_IP=$1
 test -z $2 || INTERFACE=$2
 iptables -t nat -A PREROUTING -i ${INTERFACE} -p tcp --dport 5347 -j DNAT --to ${TARGET_IP}:5347
+iptables -t nat -A PREROUTING -i ${INTERFACE} -p tcp --dport 5280 -j DNAT --to ${TARGET_IP}:5347
 iptables -t nat -A PREROUTING -i ${INTERFACE} -p tcp --dport 80 -j DNAT --to ${TARGET_IP}:80
 iptables -t nat -A PREROUTING -i ${INTERFACE} -p tcp --dport 443 -j DNAT --to ${TARGET_IP}:443
-iptables -t nat -A PREROUTING -i ${INTERFACE} -p udp --dport 10000:10010 -j DNAT --to ${TARGET_IP}:10000-10010
+iptables -t nat -A PREROUTING -i ${INTERFACE} -p udp --dport 10000:10020 -j DNAT --to ${TARGET_IP}:10000-10020
